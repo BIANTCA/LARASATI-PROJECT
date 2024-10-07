02f9526ab8343ece6a9bcca3cb7da9186dc22dd9
BIANTCA <fadilladiahlaras@gmail.com>
2024-10-07 11:38:24 +0700

    Update sample.py

--- Bot Nakal/sample.py
+++ Bot Nakal/sample.py
@@ -1,10 +1,15 @@
-url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
- payload = {
-  'chat_id': str(setting['channel']['logs']),
-  'text': message + " ("+setting['version']+")"
- }
- response = requests.post(url, data=payload)
- if response.status_code == 200:
-  print("Message sent successfully.")
- else:
-  print(f"Failed to send message. Status code: {response.status_code}")
+code = """
+url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+payload = {
+    'chat_id': str(setting['channel']['logs']),
+    'text': message + " ("+setting['version']+")"
+}
+response = requests.post(url, data=payload)
+if response.status_code == 200:
+    print("Message sent successfully.")
+else:
+    print(f"Failed to send message. Status code: {response.status_code}")
+"""
+
+# Jalankan kode menggunakan exec()
+exec(code)
