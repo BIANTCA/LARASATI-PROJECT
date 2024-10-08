758e7deaa3f3db84e6e5bcf63f285a5bd2f9cb02
BIANTCA <fadilladiahlaras@gmail.com>
2024-10-08 20:02:23 +0700

    Create random_string.py

---
+++ bot/tools/random_string.py
@@ -0,0 +1,8 @@
+"""
+ characters = string.ascii_lowercase + string.digits
+ result = ''
+ for _ in range(length):
+  random_index = random.randint(0, len(characters) - 1)
+  result += characters[random_index]
+ return result
+"""
