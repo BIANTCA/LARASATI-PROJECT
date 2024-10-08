150fb9f0c2c372ca485651e904d3086b6087832f
BIANTCA <fadilladiahlaras@gmail.com>
2024-10-08 19:20:32 +0700

    Update readfile.py

--- bot/tools/readfile.py
+++ bot/tools/readfile.py
@@ -1,4 +1,6 @@
-```if not os.path.exists(path):
+"""
+if not os.path.exists(path):
   return None
 with open(path) as json_file:
-  return json.load(json_file)```
+  return json.load(json_file)
+"""
