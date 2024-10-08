f171dd6d73fb742307766ac0ddca47cda2407849
BIANTCA <fadilladiahlaras@gmail.com>
2024-10-08 19:41:37 +0700

    Update send_message.py

--- bot/tools/send_message.py
+++ bot/tools/send_message.py
@@ -1,9 +1,7 @@
-{
-    "code": "url = f\"https://api.telegram.org/bot{bot_token}/sendMessage\"\n"
-            "payload = {'chat_id': chat_id, 'text': message}\n"
-            "response = requests.post(url, data=payload)\n"
-            "if response.status_code == 200:\n"
-            "    print(\"Message sent successfully.\")\n"
-            "else:\n"
-            "    print(f\"Failed to send message. Status code: {response.status_code}\")"
-}
+"url = f\"https://api.telegram.org/bot{bot_token}/sendMessage\"\n"
+"payload = {'chat_id': chat_id, 'text': message}\n"
+"response = requests.post(url, data=payload)\n"
+"if response.status_code == 200:\n"
+"    print(\"Message sent successfully.\")\n"
+"else:\n"
+"    print(f\"Failed to send message. Status code: {response.status_code}\")"
