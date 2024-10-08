5ce7bdd5e07195045f7723d651946586f0a17ef9
BIANTCA <fadilladiahlaras@gmail.com>
2024-10-08 20:03:38 +0700

    Update random_string.py

--- bot/tools/random_string.py
+++ bot/tools/random_string.py
@@ -1,8 +1,6 @@
-"""
- characters = string.ascii_lowercase + string.digits
- result = ''
- for _ in range(length):
-  random_index = random.randint(0, len(characters) - 1)
-  result += characters[random_index]
+characters = string.ascii_lowercase + string.digits
+result = ''
+for _ in range(length):
+ random_index = random.randint(0, len(characters) - 1)
+ result += characters[random_index]
  return result
-"""
